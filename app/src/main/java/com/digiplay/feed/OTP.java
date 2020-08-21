88da0b0e1f401ebc25e6ba0f7b959b025e1d8666
Ndabezinhle21 <ndabezinhlenyathi21@gmail.com>
2020-08-21 20:25:25 +0200

    Hackathon ready

--- app/src/main/java/com/digiplay/feed/OTP.java
+++ app/src/main/java/com/digiplay/feed/OTP.java
@@ -57,7 +57,6 @@ public class OTP extends AppCompatActivity {
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
             Intent intent = new Intent(this, HomeSwitcher.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
             startActivity(intent);
         }
     }
